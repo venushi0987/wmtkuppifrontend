14cd06b17b8e89329a3fbdb08febe1ec633fd9af
IT24103206 <it24103206@my.sliit.lk>
2026-04-27 10:59:52 +0530

    Update backend URL to Railway

--- kuppt frontend/src/api.js
+++ kuppt frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5001/api'
+  baseURL: 'https://wmtkuppibackend-production.up.railway.app'
 });
 
 export const getItems = () => API.get('/items');
