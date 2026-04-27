1419ecff1eae82ec21704355abbbb4217f7fff1f
IT24103206 <it24103206@my.sliit.lk>
2026-04-27 11:17:47 +0530

    add front end files

--- kuppt frontend/src/api.js
+++ kuppt frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://wmtkuppibackend-production.up.railway.app/api/items',
+  baseURL: 'https://wmtkuppibackend-production.up.railway.app/api',
 });
 
 export const getItems = () => API.get('/items');
